041cb86bb04b22245c218064c0459e30cf01050f
komalddeshmukh <111448795+komalddeshmukh@users.noreply.github.com>
2022-08-20 16:30:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//1. For the given  JSON iterate over all for loop{ for, forEach, for in, for of}
+
+
+//let object=[
+    //{"person":"John","age":30,"company":"Google"},
+    //{"person":"Mohn","age":37,"company":"Flipkart"},
+    //{"person":"Kohn","age":38,"company":"Amazon"},
+    //{"person":"Sohan","age":40,"company":"Myntra"},
+    //{"person":"Jenny","age":70,"company":"Apple"},
+//];
+
+//for(let i=0; i<object.length; i++ )
+//{
+  //  console.log(object[i]);
+//}
+
+//object.forEach((student)=>
+ //   {
+//console.log(student);
+   // })
+
+//for(let student in object){
+ //   console.log(object[student]);
+//}
+
+//for (let student of object){
+  //  console.log(student);
+//}
+
+
+//2.creat your own resume in JSON format
+
+//let person={
+  //  "name":"Komal",
+   // "Age": 26,
+   // "EmailId":"komal784@gmail.com" ,
+   // "City": "Nashik",
+   // "Qualification":"BE Electrical",
+   // "PassingYear":"2018",
+   // "Experiance": 4,
+   // "Designation":"Skill Development Officer",
+    //"OrganizationName":"Bosch India Foundation",
+//}
+//console.log(person);
+
+
